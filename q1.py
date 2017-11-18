e5cb51238d3b8e579f332796583ebdf748133ce7
kudos3939 <annie1020k@gmail.com>
2017-11-18 20:58:13 +0900

    removed redundant code

--- q1.py
+++ q1.py
@@ -3,16 +3,12 @@
 
 def q1():
     maximum = 1000
-    three_five = []
+    result = 0
     for x in range(0, maximum):
         if x % 3 == 0 or x % 5 == 0:
-            three_five.append(x)
+            result += x
 
-    sum = 0
-    for x in three_five:
-        sum += x
-
-    print sum
+    print result
 
 
 q1()
