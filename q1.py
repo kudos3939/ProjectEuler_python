c1bdf02984fd7bf46c2a7a57feae9e790fe58611
kudos3939 <annie1020k@gmail.com>
2017-11-18 22:14:03 +0900

    added comment in q1

--- q1.py
+++ q1.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python
+# coding: UTF-8
 
 
+# 1000未満の全ての3の倍数と5の倍数の合計を出力します。
 def q1():
-    maximum = 1000
-    result = 0
+    maximum = 1000  # 最大値
+    result = 0  # 結果を格納するための変数
+    # 0から999まで順番にxに代入していきます。
     for x in range(0, maximum):
+        # xが3の倍数、または5の倍数であるかチェックします。
         if x % 3 == 0 or x % 5 == 0:
-            result += x
+            result += x  # チェックがTrueを返したら変数に加えます。
 
-    print result
+    print result  # コンソールへ出力します。
 
 
+# q1を呼び出しています。プログラムはここから始まります。
 q1()
