f3d5557423933b9d3f6eb3f36b2596a1893a7803
kudos3939 <annie1020k@gmail.com>
2017-11-21 14:04:05 +0900

    updated q7 to use util.find_primes()

--- q7.py
+++ q7.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python
 # coding: utf-8
-from util import is_divisible
+from util import find_primes
 
 
 # 10001番目の素数を求めます。
 def q7():
+    length = 0  # 帰ってきた素数リストの長さを格納するための変数です。
+    a_number = 10000  # まず最初は10000までの素数を求めます。
+    while length < 10001:  # リストの長さが10001以上になったらループを抜けます。
+        prime_numbers = find_primes(a_number)
+        length = len(prime_numbers)
+        a_number += a_number  # 次のループに備えて数を2倍にします。
 
-    prime_numbers = [2]  # 最小の素数を予め入れとく
-    counter = 1  # 何番目の素数か数えてくれます。2がもう入っているので1から始まります。
-    n_number = 2  # 試される数です。ループの最初でインクリメントするので2からになります。(最初に試される数は3）
-    while counter < 10001:  # カウンターが10001になったらループを抜けます。
-        n_number += 1
-        if not is_divisible(n_number, *prime_numbers):
-            prime_numbers.append(n_number)
-            counter += 1
-    print n_number
+    print prime_numbers[10000]  # 先頭は0なので10000の要素をプリントします。
 
 
 q7()
