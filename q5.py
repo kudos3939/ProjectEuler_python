17d1557772a2ab8298592ce91304727fd782dd0c
kudos3939 <annie1020k@gmail.com>
2017-11-21 13:41:20 +0900

    updated q5 to use util.find_primes()

--- q5.py
+++ q5.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # coding: utf-8
-from util import is_divisible  # モジュールをインポートします。
+from util import find_primes  # モジュールをインポートします。
 """
 ※ 実際にコードを試してみたい方はq5.pyとutil.pyを同じディレクトリに配置してください。
 """
@@ -17,17 +17,7 @@ def q5():
         while biggest_power * p < 20:
             biggest_power *= p
         smallest_multiple *= biggest_power  # 最小公倍数の変数にかけ合わせます。
-    smallest_multiple = 1
     print smallest_multiple
-
-
-# 与えられた引数までの素数を求め、配列を返します。
-def find_primes(max_number):
-    prime_numbers = [2]  # 空だとis_divisibleを呼び出した時エラーになるので2を予め入れています。
-    for x in range(3, max_number):  # 0からだと0と1も入ってしまい、2がダブります。
-        if is_divisible(x, prime_numbers):  # importした関数を呼び出しています。
-            prime_numbers.append(x)
-    return prime_numbers
 
 
 q5()
