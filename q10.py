5186faa5dd43c5c49cd1cd4c5c6bff6bf40b41f2
kudos3939 <annie1020k@gmail.com>
2017-11-21 14:18:58 +0900

    added comments

--- q10.py
+++ q10.py
@@ -3,9 +3,11 @@
 from util import find_primes
 
 
+# 200万以下の素数を全て足した合計を出します。
 def q10():
     prime_numbers = find_primes(2000000)
 
+    # reduce関数に隣り合ったリストの要素を足し合わせるラムダ式と素数のリストを渡します。
     print reduce(lambda a, b: a + b, prime_numbers)
 
 
--- util.py
+++ util.py
@@ -4,11 +4,12 @@ from math import sqrt
 from collections import deque
 
 
+# 素数の可能性が高い数字だけに絞り込みリストにします。
 def create_wheel(maximum):
-    primes = [2, 3, 5]
-    step = [6, 4, 2, 4, 2, 4, 6, 2]
+    primes = [2, 3, 5]  # 一つのサイクルは30です。
+    step = [6, 4, 2, 4, 2, 4, 6, 2]  # 一つのサイクル内で素数が出現する可能性のあるステップ
 
-    wheel = []
+    wheel = []  # 素数だと思われる数字を書くのするリスト
     a_number = 1
     while a_number < maximum:
         for n in step:
@@ -16,14 +17,16 @@ def create_wheel(maximum):
             if a_number > maximum:
                 break
             wheel.append(a_number)
-    return primes + wheel
+    return primes + wheel  # 求めたリストと最初の素数リストを足して返します。
 
 
+# 求めたwheel(素数の可能性が高い数字群)をSieve of Eratosthenesを用いて絞り込みます。
 def sieve_prime(wheel):
     prime_numbers = []
     wheel = deque(wheel)
     while wheel[0] < sqrt(wheel[-1]):
         head = wheel.popleft()
+        # filter関数でラムダ式に該当する要素だけを抽出します。
         wheel = deque(filter(lambda p: p % head != 0, wheel))
         prime_numbers.append(head)
     prime_numbers += list(wheel)
@@ -31,6 +34,7 @@ def sieve_prime(wheel):
     return prime_numbers
 
 
+# wheel factorisation を用いて素数を求めます。
 def find_primes(maximum):
     wheel = create_wheel(maximum)
     return sieve_prime(wheel)
