49282c92b937061f9ec25354ed7ec052c0847734
kudos3939 <annie1020k@gmail.com>
2017-11-25 21:39:08 +0900

    q13 done! not commented

---
+++ q13.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# coding: utf-8
+
+
+def q13():
+    f = open("q13_number", "r")
+    numbers = f.read().split()
+
+    result = reduce(lambda a, b: str_sum(a, b), numbers)
+
+    print result[:10]
+
+
+def one_str_sum(first, second, carry):
+    result = int(first) + int(second)
+    if carry:
+        result += 1
+    result = str(result)
+    if len(result) == 2:
+        return result[1], True
+    else:
+        return result, False
+
+
+def str_sum(first, second):
+    result = ''
+    first = list(first)
+    second = list(second)
+    carry = False
+    while len(first) and len(second) != 0:
+        one_str, carry = one_str_sum(first.pop(), second.pop(), carry)
+        result = one_str + result
+
+    while len(first) != 0:
+        one_str, carry = one_str_sum(first.pop(), '0', carry)
+        result = one_str + result
+    while len(second) != 0:
+        one_str, carry = one_str_sum('0', second.pop(), carry)
+        result = one_str + result
+    if carry:
+        result = '1' + result
+    return result
+
+
+q13()
