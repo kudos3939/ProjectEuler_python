676e5b96fbd38e84643f760757db1ecc2df71156
kudos3939 <annie1020k@gmail.com>
2017-11-26 17:02:06 +0900

    q14 optimised

--- q14.py
+++ q14.py
@@ -3,19 +3,25 @@
 
 
 def q14():
-    longest_chain = []
+    known_numbers = {}
+    longest_chain = (0, 0)
     for n in range(1, 1000000):
-        chain = []
+        num = n
+        chain = 1
         while n != 1:
-            chain.append(n)
+            if n in known_numbers:
+                chain += known_numbers[n] - 1
+                break
+            chain += 1
             if n % 2 == 0:
                 n /= 2
             else:
                 n = n * 3 + 1
-        chain.append(n)
 
-        if len(chain) > len(longest_chain):
-            longest_chain = chain
+        if num not in known_numbers:
+            known_numbers[num] = chain
+        if chain > longest_chain[1]:
+            longest_chain = (num, chain)
 
     print longest_chain
 
