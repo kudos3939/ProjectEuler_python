fad34e0422ead04fde2b8fc853dfc7e80e1b4212
kudos3939 <annie1020k@gmail.com>
2017-12-28 18:49:04 +0900

    q18 and q67 done!

--- q18.py
+++ q18.py
@@ -4,18 +4,26 @@ from collections import deque
 
 
 def q18():
-    f = open('q18_number', 'r')
+    f = open('q67_number', 'r')
     # 数字をノード化しデックに納めます。
     num_of_line = 0
     nodes = []
+    the_sum = 0
     for line in f.readlines():
-        line = [Node(num) for num in line.split()]
+        line = [Node(int(num)) for num in line.split()]
         nodes.append(line)
         num_of_line += 1
 
-    for i in range(num_of_line):
-        for j in range(i):
-            nodes[j]
+    for n in range(num_of_line - 1):
+        for j in range(len(nodes[n])):
+            nodes[n+1][j].set_value(nodes[n][j])
+            nodes[n+1][j+1].set_value(nodes[n][j])
+
+    for n in nodes[num_of_line - 1]:
+        if the_sum < n.get_value():
+            the_sum = n.get_value()
+
+    print the_sum
 
 
 class Node:
@@ -25,15 +33,15 @@ class Node:
 
     def __init__(self, value):
         self.__value = value
-
-    def __lt__(self, other):
-        return self.__total_value < other.get_value()
-
-    def retrieve_path(self):
-        return 0
+        self.__total_value = value
 
     def get_value(self):
         return self.__total_value
+
+    def set_value(self, node):
+        if self.__total_value < node.get_value() + self.__value:
+            self.__total_value = node.get_value() + self.__value
+            self.__path = node
 
     def __repr__(self):
         return str(self.__value)
