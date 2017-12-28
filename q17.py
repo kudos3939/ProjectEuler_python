3cb9778359aa25e3df41e71ae81ecaddefc62fdb
kudos3939 <annie1020k@gmail.com>
2017-12-28 18:51:19 +0900

    capitalised consts

--- q17.py
+++ q17.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # coding: utf-8
-numbers = {'0': '', '1': 'one', '2': 'two', '3': 'three', '4': 'four', '5': 'five', '6': 'six', '7': 'seven',
+NUMBERS = {'0': '', '1': 'one', '2': 'two', '3': 'three', '4': 'four', '5': 'five', '6': 'six', '7': 'seven',
            '8': 'eight', '9': 'nine', '10': 'ten', '11': 'eleven', '12': 'twelve', '13': 'thirteen', '14': 'fourteen',
            '15': 'fifteen', '16': 'sixteen', '17': 'seventeen', '18': 'eighteen', '19': 'nineteen'}
-tens = {'2': 'twenty', '3': 'thirty', '4': 'forty', '5': 'fifty', '6': 'sixty', '7': 'seventy', '8': 'eighty',
+TENS = {'2': 'twenty', '3': 'thirty', '4': 'forty', '5': 'fifty', '6': 'sixty', '7': 'seventy', '8': 'eighty',
         '9': 'ninety'}
 
 
@@ -16,15 +16,15 @@ def q17():
 
 def convert_to_eng(digits):
     if digits < 20:
-        return numbers[str(digits)]
+        return NUMBERS[str(digits)]
     elif digits < 100:
         str_digits = str(digits)
-        return tens[str_digits[0]] + numbers[str_digits[1]]
+        return TENS[str_digits[0]] + NUMBERS[str_digits[1]]
     elif digits < 1000:
         str_digits = str(digits)
         if digits % 100 == 0:
-            return numbers[str_digits[0]] + 'hundred'
-        return numbers[str_digits[0]] + 'hundredand' + convert_to_eng(int(str_digits[1:]))
+            return NUMBERS[str_digits[0]] + 'hundred'
+        return NUMBERS[str_digits[0]] + 'hundredand' + convert_to_eng(int(str_digits[1:]))
     elif digits == 1000:
         return 'onethousand'
 
