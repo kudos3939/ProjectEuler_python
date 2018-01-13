3e6351bbbbae4f64a1a16eb607765c2651ce964f
kudos3939 <annie1020k@gmail.com>
2018-01-13 23:31:07 +0900

    q21 optimised used q12 strategy

--- q21~q30/q21.py
+++ q21~q30/q21.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 # coding: utf-8
 from util import find_primes
+from math import floor, sqrt
 
 
 def q21():
     exception = find_primes(10000)
     total = 0
-    for num in range(1, 10000):
+    for num in range(2, 10000):
         if num in exception:
             continue
         ano_num = sum_of_proper_divisor(num)
@@ -19,11 +20,16 @@ def q21():
 
 
 def sum_of_proper_divisor(num):
-    result = 0
-    for n in range(1, num):
-        if num % n is 0:
-            result += n
-    return result
+    divisors = []
+    for n in range(1, int(floor(sqrt(num) + 1))):
+        if num % n == 0:
+            divisors.append(n)
+    rest_divisors = [num / x for x in divisors]
+    for r in rest_divisors:
+        if r not in divisors:
+            divisors.append(r)
+    divisors.remove(num)
+    return reduce(lambda a, b: a+b, divisors)
 
 
 q21()
