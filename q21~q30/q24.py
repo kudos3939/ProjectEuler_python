a6a117499ffd88ab3ca120e0e981b2f28fa770c4
kudos3939 <annie1020k@gmail.com>
2018-01-24 08:41:44 +0900

    q24 done!(but not satisfied really)

---
+++ q21~q30/q24.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# coding: utf-8
+
+
+def q24():
+    numbers = range(10)
+    str_numbers = [str(n) for n in numbers]
+    perms = []
+    find_perms(str_numbers, '', perms)
+
+    perms.sort()
+
+    print(perms[999999])
+
+
+def find_perms(nums, digits, perms=[]):
+    if len(nums) is 0:
+        perms.append(digits)
+    for x in nums:
+        new_nums = [r for r in nums if r != x in nums]
+        find_perms(new_nums, digits + x, perms)
+
+
+q24()
