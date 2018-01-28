e44be4bc0e16a4dfb9e4908d101b7651a5cf5215
kudos3939 <annie1020k@gmail.com>
2018-01-28 23:46:49 +0900

    q28 done!

--- q21~q30/q28.py
+++ q21~q30/q28.py
@@ -1,8 +1,16 @@
 #!/usr/local/bin/python3
 
 
-def q28():
-    print()
+def q28(limit):
+    gap = 2
+    total = 1
+    num = 1
+    while gap < limit:
+        for n in range(4):
+            num += gap
+            total += num
+        gap += 2
+    print(total)
 
 
-q28()
+q28(1001)
